dd42b00094121397931d3e39d09393b20ce84a56
Joshua Forbes <89933776+JoshJforbes@users.noreply.github.com>
2026-01-31 00:14:48 -0500

    added random color functionality to sketch pad

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const sketchPad = document.querySelector(".sketch-container");
 
-let squareCount = 16;
-let squaresPerRow = 4;
+let squareCount = 100;
+let squaresPerRow = 10;
 
 for (let i = 0; i < squareCount; i++) {
     let sketchPadWidth = sketchPad.clientWidth;
@@ -18,3 +18,19 @@ for (let i = 0; i < squareCount; i++) {
 
     sketchPad.appendChild(gridSquare);
 }
+
+sketchPad.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = generateRandomColor();
+})
+
+const generateRandomColor = () => {
+    const hexPool = "0123456789ABCDEF"
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += hexPool.charAt(Math.floor(Math.random() * (hexPool.length - 1)));
+    }
+
+    return color;
+}
+
